ca69d39f48982986cc85b9030cfbbb67c8c5006e
dimonet78 <38493429+dimonet78@users.noreply.github.com>
2025-07-28 20:02:27 +0200

    up

--- Pruebas/frontend.py
+++ Pruebas/frontend.py
@@ -22,15 +22,55 @@ class ScreenCaptureWidget(QWidget):
         self.label = QLabel('')
         self.label.setAlignment(Qt.AlignCenter)
 
+
+        # Área de datos y piloto
+        self.data_label = QLabel('Esperando datos...')
+        self.data_label.setAlignment(Qt.AlignCenter)
+        self.pilot_label = QLabel()
+        self.pilot_label.setFixedSize(30, 30)
+        self.pilot_label.setAlignment(Qt.AlignCenter)
+        self.update_pilot(False)
+
         layout = QVBoxLayout()
         layout.addWidget(self.combo)
         layout.addWidget(self.label)
+        layout.addWidget(self.data_label)
+        layout.addWidget(self.pilot_label)
         self.setLayout(layout)
 
         self.selected_monitor = 0
+        self.is_racing_game = False
         self.timer = QTimer()
         self.timer.timeout.connect(self.capture_screen)
         self.timer.start(50)  # 20 fps aprox
+
+        # Simulación de detección de juego de coches (alternar cada 2 seg)
+        self.detect_timer = QTimer()
+        self.detect_timer.timeout.connect(self.simulate_detection)
+        self.detect_timer.start(2000)
+
+    def update_pilot(self, is_racing):
+        # Dibuja un círculo verde o rojo
+        color = 'green' if is_racing else 'red'
+        pixmap = QPixmap(30, 30)
+        pixmap.fill(Qt.transparent)
+        from PyQt5.QtGui import QPainter, QColor
+        painter = QPainter(pixmap)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setBrush(QColor(color))
+        painter.setPen(Qt.NoPen)
+        painter.drawEllipse(5, 5, 20, 20)
+        painter.end()
+        self.pilot_label.setPixmap(pixmap)
+
+    def simulate_detection(self):
+        # Simula la detección alternando el estado
+        self.is_racing_game = not self.is_racing_game
+        self.update_pilot(self.is_racing_game)
+        if self.is_racing_game:
+            self.data_label.setText('Juego de coches detectado')
+        else:
+            self.data_label.setText('No es un juego de coches')
 
     def update_monitor(self, idx):
         self.selected_monitor = idx
